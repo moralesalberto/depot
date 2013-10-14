fe08701a5c5786f1309ed7a3803e44b22df93611
moralesalberto <alberto@moralitos.com>
2013-10-14 09:31:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Depot Application
+-----------------
+
+This is an exercise of taking the rails Depot application
+and writing it with Cucumber features
+
+The first step is to create the rails application without 
+the unit tests
+
+```
+$ rails new depot --skip-test-unit
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '4c252ed569294c9a246871452265121bf206428435fa131be39899b1f856f9430d13b8790bf789cb701823727d76a12ca7e53625e8bc798a84aad644cf1361a3'
